1eb17d4251cc6658903dc5658fda1f36f4e8f89d
tsmx <dev@tsmx.net>
2025-01-13 22:20:25 +0100

    Added NodeJS 22

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { getEnv } = require('./functions/function');
 
 const httpPort = process.env.PORT || 3030;
 
-app.get('/', (req, res) => { getEnv(req, res); });
-
-app.listen(httpPort, () => { console.log('Listening on port ' + httpPort + '...'); });
+if (process.env['GAE_RUNTIME']) {
+    app.get('/', (req, res) => { getEnv(req, res); });
+    app.listen(httpPort, () => { console.log('Listening on port ' + httpPort + '...'); });
+}
