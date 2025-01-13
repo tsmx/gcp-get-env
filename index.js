705551200832ffd33bbb5dcec257fdc5804648af
tsmx <dev@tsmx.net>
2025-01-13 23:06:51 +0100

    NodeJS 22 added for Cloud Functions

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { getEnv } = require('./functions/function');
 
 const httpPort = process.env.PORT || 3030;
 
-if (process.env['GAE_RUNTIME']) {
+const functions = require('@google-cloud/functions-framework');
+functions.http('getEnv2', (req, res) => { getEnv(req, res); });
+
+if (process.env['GAE_RUNTIME'] && process.env['GAE_SERVICE']) {
     app.get('/', (req, res) => { getEnv(req, res); });
     app.listen(httpPort, () => { console.log('Listening on port ' + httpPort + '...'); });
 }
